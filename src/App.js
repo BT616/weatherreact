a53590017808276620bb117e42bd26162eeefea5
BT616 <110855674+BT616@users.noreply.github.com>
2022-12-05 12:31:37 -0800

    css, hello on page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Search from './components/search/search';
 
 function App() {
   return (
-    <div className='App'>
+    <div className='container'>
       <Search/>
    
     </div>
